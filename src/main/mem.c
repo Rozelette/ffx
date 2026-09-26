867d3b6c16130f293f584fec442bbbf66813ecb8
Rozelette <Uberpanzermensch@gmail.com>
2026-09-26 02:50:36 -0500

    Set up mem.c and mem2.c

---
+++ src/main/mem.c
@@ -0,0 +1,124 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011B9E8);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011BA28);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011BA80);
+
+INCLUDE_RODATA("asm/nonmatchings/main/mem", D_005503C0);
+
+INCLUDE_RODATA("asm/nonmatchings/main/mem", D_005503C8);
+
+INCLUDE_RODATA("asm/nonmatchings/main/mem", D_005503D0);
+
+INCLUDE_RODATA("asm/nonmatchings/main/mem", D_005503D8);
+
+INCLUDE_RODATA("asm/nonmatchings/main/mem", D_005503E0);
+
+INCLUDE_RODATA("asm/nonmatchings/main/mem", D_005503E8);
+
+INCLUDE_RODATA("asm/nonmatchings/main/mem", D_005503F0);
+
+INCLUDE_RODATA("asm/nonmatchings/main/mem", D_005503F8);
+
+INCLUDE_RODATA("asm/nonmatchings/main/mem", D_00550400);
+
+INCLUDE_RODATA("asm/nonmatchings/main/mem", D_00550408);
+
+INCLUDE_RODATA("asm/nonmatchings/main/mem", D_00550410);
+
+INCLUDE_RODATA("asm/nonmatchings/main/mem", D_00550418);
+
+INCLUDE_RODATA("asm/nonmatchings/main/mem", D_00550420);
+
+INCLUDE_RODATA("asm/nonmatchings/main/mem", D_00550428);
+
+INCLUDE_RODATA("asm/nonmatchings/main/mem", D_00550430);
+
+INCLUDE_RODATA("asm/nonmatchings/main/mem", D_00550438);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011BAD0);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011BCD0);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011BD38);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011BD48);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011BD58);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011BE10);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011BEA8);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011BEC0);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011BF08);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011BF18);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011BF98);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011BFA8);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011BFC8);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011C078);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011C0C8);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011C108);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011C150);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011C1A0);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011C2F0);
+
+void func_0011C308(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011C310);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011C320);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011C6A8);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011C7B0);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011C800);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011C880);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011C918);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011C9D0);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011C9F0);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011CAA8);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011CAD0);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011CAF8);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011CB80);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011CBE0);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011CC40);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011CCF8);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011CD88);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011CDB0);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011CF80);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011CFA8);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011CFD0);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem", func_0011D140);
---
+++ src/main/mem2.c
@@ -0,0 +1,42 @@
+#include "common.h"
+
+void func_0011D1F0(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/main/mem2", func_0011D1F8);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem2", func_0011D338);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem2", func_0011D3E8);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem2", func_0011D480);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem2", func_0011D530);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem2", func_0011D558);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem2", func_0011D560);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem2", func_0011D588);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem2", func_0011D5B0);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem2", func_0011D5B8);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem2", func_0011D5E0);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem2", func_0011D608);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem2", func_0011D640);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem2", func_0011D690);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem2", func_0011D698);
+
+INCLUDE_ASM("asm/nonmatchings/main/mem2", func_0011D6E0);
+
+INCLUDE_RODATA("asm/nonmatchings/main/mem2", D_00550610);
+
+INCLUDE_RODATA("asm/nonmatchings/main/mem2", D_00550618);
+
+INCLUDE_RODATA("asm/nonmatchings/main/mem2", D_00550620);
